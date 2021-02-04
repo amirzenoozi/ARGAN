de474358b52b7184d8d0836212b12c2f110459e1
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2021-02-04 12:45:14 +0330

    :zap: remove clearSession

--- tools/mobile_net.py
+++ tools/mobile_net.py
@@ -1,6 +1,5 @@
 from tensorflow.keras.layers import Input
 import tensorflow.keras.applications as kerasApp
-import tensorflow.keras.backend as K
 import tensorflow as tf
 import time
 
@@ -27,8 +26,6 @@ class MobileNet:
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
         
         bgr = tf.concat(axis=3, values=[blue - BGR_MEAN[0], green - BGR_MEAN[1], red - BGR_MEAN[2]])
-        # bgr = tf.image.resize_images(bgr, [224, 224])
-        K.clear_session()
         model = kerasApp.MobileNet(input_shape=None, alpha=1.0, depth_multiplier=1, dropout=0.001, include_top=False, weights="imagenet", input_tensor=Input(shape=(256, 256, 3)), pooling=None, classes=1000)
         prediction = model.predict( bgr, steps=1 )
         self.no_activation_layer = model.get_layer('conv_dw_1_bn').output
